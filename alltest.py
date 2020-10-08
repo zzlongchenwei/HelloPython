7a13e5256c14940069868e65ee7a791a51abd907
zzlong <1071707383@qq.com>
2020-10-08 21:33:47 +0800

    修改了第一题的列表数字转列表字符串，通过闭包方式让任意数字的组合

---
+++ alltest.py
@@ -0,0 +1,13 @@
+"""
+-剑衣沉沉晚霞归，酒杖津津神仙来-
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+@time:2020/10/08  @author:zzlong  
+@file:alltest.py
+"""
+print(list(range(1,4)))
+l = [1,2,3]
+st = []
+for i in l:
+    st.append(str(i))
+print(st)
--- python100/题目1.py
+++ python100/题目1.py
@@ -17,15 +17,33 @@ for i in range(1, 5):
 print(numlist)
 
 
-def comNumber(num='', figure=3):
-    l = ['1', '2', '3', '4']
-    for i in l:
-        if i not in num:
-            if len(num) == figure-1 :
-                yield i
-            else:
-                for j in comNumber(num+i):
-                    yield i+j
+def NumToCh(startnum,endnum):
+    # 将数字列表转为字符串列表，
+    # startnum为列表起始数字
+    # endnum为终止数字
+    ch = []
+    for i in range(startnum, endnum+1):
+        ch.append(str(i))
+    return ch
 
 
-print(list(comNumber()))
+def comNumber(startnum=1,endnum=4,figure=3):
+    # startnum起始数字，endnum终止数字，figure几位数字组合
+    l = NumToCh(startnum,endnum)
+
+    def nextNumber(chnum=''):
+        # 递归部分
+        for i in l:
+            if i not in chnum:
+                # 如果该数字不在chnum中则继续
+                if len(chnum) == figure-1 :
+                    # 如果是最后一个数字，就生成该数字
+                    yield i
+                else:
+                    for j in nextNumber(chnum+i):
+                        # 递归数字
+                        yield i+j
+    return nextNumber
+
+
+print(list(comNumber()()))
