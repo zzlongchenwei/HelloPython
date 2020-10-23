e8376ba68f7551b3a20c44175a9d8b55bb862b18
zzlongchenwei <1071707383@qq.com>
2020-10-23 17:30:58 +0800

    将重新改写的GA加入

--- GenerationAlgorithm/main.py
+++ GenerationAlgorithm/main.py
@@ -6,50 +6,56 @@
 @file:main.py
 """
 
-from population import Population
+from GA import GA
 from stopping_rule import stop_rule
+from plot import *
+if __name__ == '__main__':
+    interval = [-10, 10]  # 区间
+    mod = 'min'  # 模式
 
-interval = [-10, 10]  # 区间
-mod = 'min'  # 模式
+    func = lambda x: x**2
+    # 产生一个种群
+    ga = GA(func, mod, 1, interval, 20)  # 一个种群实例
+    ga.produce_rpop()  # 创建一个实数编码种群
 
+    plotfunc(func, ga.pop)
+    # print('当前种群', ga.pop)  # 打印该种群
+    # print('----' * 10)
+    while stop_rule(ga.age, NG=100):
+        print('========>第%s代开始' % ga.age)
+        print('当前种群', ga.pop)
+        print('-----' * 10)
 
-def func(x):
-    return x ** 2
+        # 交叉
+        ga.crossover(op_alph=1)
+        print('----' * 10)
 
+        # 变异
+        ga.mutation()
+        print('----' * 10)
 
-# 产生一个种群
-ga = Population(func, mod, 1, interval, 20)  # 一个种群实例
-ga.produce_rpop()  # 创建一个是实数编码种群
-# print('当前种群', ga.pop)  # 打印该种群
-# print('----' * 10)
-while stop_rule(ga.age, NG=150):
-    print('第%s代' % ga.age)
-    print('当前种群', ga.pop)
-    print('-----' * 10)
+        # 选择
+        ga.pro_fitness()  # 计算适应值
 
-    # 交叉
-    ga.crossover(op_alph=1)
-    print('----' * 10)
+        print('----' * 10)
+        ga.rws()  # 轮盘赌选择
+        print('----' * 10)
 
-    # 变异
-    ga.mutation()
-    print('----' * 10)
+        # 更新种群
+        ga.update_pop()
+        print('===========>第%s代结束' % ga.age)
+        print('------------------'*10)
 
-    # 选择
-    ga.pro_fitness()  # 计算适应值
+        ga.age += 1
 
-    print('----' * 10)
-    ga.rws()  # 轮盘赌选择
-    print('----' * 10)
+        # 画图
+        plotfunc(func, ga.pop)
 
-    # 更新种群
-    ga.update_pop()
+    print('迭代%s完成' % ga.age)
 
-    ga.age += 1
-
-print('迭代%s完成' % ga.age)
-sort_lits = sorted(map(abs, ga.pop))
-print('排序后', sort_lits)
-min = func(sort_lits[0])
-print('最优解', sort_lits[0])
-print('最优值', min)
+    sort_lits = sorted(map(abs, ga.pop))
+    print('排序后', sort_lits)
+    min = func(sort_lits[0])
+    print('最优解', sort_lits[0])
+    print('最优值', min)
+    plotfunc(func, ga.pop, 0)
