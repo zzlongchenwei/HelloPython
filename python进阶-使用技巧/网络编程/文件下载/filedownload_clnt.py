aa00b850a5802f59c941b834e193b5e653904305
zzlong <1071707383@qq.com>
2020-10-30 20:45:01 +0800

    实现了图片的传输

--- python进阶-使用技巧/网络编程/文件下载/filedownload_clnt.py
+++ python进阶-使用技巧/网络编程/文件下载/filedownload_clnt.py
@@ -10,22 +10,29 @@
 import socket
 
 def main():
+    # 1.创建套接字
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # dest_ip = input('输入下载服务器的ip：')
+    # dest_port = input('输入下载服务器的port：')
 
-    dest_ip = input('输入下载服务器的ip：')
-    dest_port = input('输入下载服务器的port：')
-
-    tcp_socket.connect((dest_ip, int(dest_port)))
-
+    dest_ip = '127.0.0.1'
+    dest_port = 7890
+    # 2.链接客户端
+    tcp_socket.connect((dest_ip, dest_port))
+    # 3.输入要下载的文件名
     download_file_name = input('请输入要下载的文件名字：')
-
+    # 4.发送要下载的文件名
     tcp_socket.send(download_file_name.encode("utf-8"))
-
-    recv_data = tcp_socket.recv(1024)
-
-    with open("[new]" + download_file_name, "wb") as f:
-        f.write(recv_data)
-
+    # 5.等待接收数据
+    while True:
+        recv_data = tcp_socket.recv(1024)
+        # 6.保存数据
+        if recv_data:
+            with open("[new]" + download_file_name, "ab") as f:
+                f.write(recv_data)
+        else:
+            break
+    # 7.关闭套接字
     tcp_socket.close()
 
 
--- python进阶-使用技巧/网络编程/文件下载/filedownload_server.py
+++ python进阶-使用技巧/网络编程/文件下载/filedownload_server.py
@@ -8,21 +8,42 @@
 """
 import socket
 
-def main():
-    tcp_server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    tcp_server_sock.bind(("", 7890))
-
-    tcp_server_sock.listen(128)
-
-    new_clnt_sock, new_clnt_addr = tcp_server_sock.accept()
-
+def send_file_2_client(new_clnt_sock, new_clnt_addr):
+    # 1.接收客户端发送来的需要下载的文件名
     file_name = new_clnt_sock.recv(1024).decode("utf-8")
     print("客户端(%s)需要下载的文件是：%s" % (str(new_clnt_addr), file_name))
+    file_content = None
+    # 2.打开这个文件，读取数据
+    try:
+        f = open(file_name, "rb")
+        while True:
+            file_content = f.read(1024)
+            if file_content:
+                # 3.发送文件的数据给客户端
+                new_clnt_sock.senda(file_content)
+            else:
+                break
+        f.close()
+    except Exception as ret:
+        print("没有要下载的文件(%s)" % file_name)
 
-    new_clnt_sock.send(("hahhah------ok----").encode("utf-8"))
 
-    new_clnt_sock.close()
+
+def main():
+    # 1.创建套接字
+    tcp_server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 2.绑定本地信息
+    tcp_server_sock.bind(("", 7890))
+    # 3.监听
+    tcp_server_sock.listen(128)
+    while True:
+        # 4.等待客户端的链接
+        new_clnt_sock, new_clnt_addr = tcp_server_sock.accept()
+        # 5.调用文件发送函数，完成为客户端服务
+        send_file_2_client(new_clnt_sock, new_clnt_addr)
+        # 6.关闭套接字
+        new_clnt_sock.close()
+
     tcp_server_sock.close()
 
 if __name__ == '__main__':
